ddee34bb6f7f4e7b315badddcf92337040c5737e
w1ouo1w <143856486+w1ouo1w@users.noreply.github.com>
2023-09-02 10:20:01 +0800

    my first step
    2023/8/31
    2023/9/1
    2023/9/2

---
+++ 831.c
@@ -0,0 +1,313 @@
+#include <stdio.h>
+#include <string.h>
+//int main()//小写字母a
+//{
+  //  printf("%c\n",'\132');
+    //return 0;
+//}
+//int main (void)//字符个数
+//{
+  //  printf("%d\n",strlen("c:\text\32\text.c"));
+    //return 0;
+//}
+//int main()//大写字母Z
+//{
+  //  printf("%c\n",'\x61');
+    //return 0;
+
+//}
+//int main()//if语句
+//{
+   //int input=0;
+   //printf("你想当神里绫华的狗吗？(1/0)>:");
+   // scanf("%d",&input);
+   //if(input == 1)
+   // printf("是的，我想\n");
+   // else
+   // printf("不，我不想\n");
+   // return 0;
+//}
+//int main()//while循环
+//{
+//    int line = 0;
+//    printf("好好学习，天天向上\n");
+//    while(line<2000)
+//    {
+//        printf("努力中,%d\n",line);
+//        line++;
+//    }
+//    if(line>=2000);
+//    printf("取得成功\n");
+//    return 0;
+//}
+//int main(void)//加法
+//{
+//    int num1 = 0;
+//    int num2 = 0;
+//    int sum = 0;
+//    scanf("%d%d",&num1,&num2);
+//    sum = num1 + num2;
+//    printf("sum = %d\n",sum);
+//    return 0;
+
+//}
+
+//int Add(int x,int y)//自定义函数
+//{
+//    int z = x+y;
+//    return z;
+//}
+//int main(void)
+//{
+//    int a = 0;
+//    int b = 0;
+//    int sum = 0;
+//    sum = Add(a,b);
+//    printf("sum = %d",sum);
+//   return 0;
+//}
+//int main(void)//下标比数少1
+//{
+//    int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+//    int i = 0;
+//    while (i<10)
+//    {
+//        printf("%d\n",arr[i]);
+//        i++;
+//    }
+//    printf("%d\n",arr[4]);
+//return 0;
+// }
+//int Max(int x,int y)
+//{
+//    if(x > y)
+//        return x;
+//    else
+//        return y;
+//}
+
+//int main(void)
+//{
+
+
+//int num1 =0;
+//int num2 =0;
+
+//    scanf("%d%d",&num1,&num2);
+//    int num3 = Max(num1,num2);
+//    printf("%d\n",num3);
+//    return 0;
+
+
+//}
+//int main(void)
+//{
+
+
+//int arr[] = { 1 , 2 , 3 , 4 , 5 , 6 };
+//printf("%d\n",sizeof(arr));
+//return 0;
+
+//}
+//int main(void)
+//{
+//    int arr[] = { 1 , 2 , 3 , 4 , 5 , 6 };
+//    printf("%d\n",sizeof(arr)/sizeof(arr[0]));
+//    return 0;
+//}
+//int main()//负数在内存中存储的时候，存储的是二进制的补码，使用时，打印的是原码
+//{
+//    int a = 0;
+//    int b = ~a;//按位取反
+//    printf("%d\n",b);
+//
+//    return 0;
+//}
+//int main(void)
+//{
+//    int a = 10;
+//    int b = a--;
+//    printf("a = %d b = %d\n",a,b);
+//    return 0;
+//}
+//int main(void)//&&逻辑与||逻辑或
+//{
+//    int a = 3;
+//    int b = 5;
+//    int c = a && b;
+//    printf("c = %d",c);
+//
+//
+//   return 0;
+//}
+//int main(void)
+//{
+//    int a = 10;
+//    int b = 110;
+//    int max = 0;
+//    max = (a > b? a : b);
+//    printf("%d\n",max);
+//    return 0;
+//}
+//int Add(int x,int y)
+//{
+//    int z = 0;
+//    z = x + y;
+//    return z;
+//}
+//int main(void)
+//{
+//    int arr[10] = { 0 };
+//    arr[4];//[]下标引用操作符
+//    int a = 0;
+//    int b = 1;
+//    int sum = Add(a,b);//()函数引用操作符
+//    printf("%d\n",sum);
+//    return 0;
+//}
+//void test()
+//{
+//   static int a = 1;//static 修饰局部变量，局部变量生命周期变长。修饰全局变量，函数在自己所在源文件内部使用，
+//    a++;
+//        printf("a = %d\n",a);
+
+//}
+//int main()
+//{
+
+    //int a = 10;//局部变量（自动变量）（auto省略）
+    //register int a = 10;//建议把a定义成寄存器变量
+    //a = -2;//unsigned(无符号的，永远正数)
+    //struct(结构体关键字)
+     //typedf-类型定义-类型重定义
+    //typedef unsigned int u_int;
+    //unsigned int num = 20;
+    //u_int num2 = 20;
+//    int i = 0;
+//    while (i<5)
+//    {
+//        test();
+//        i++;
+//    }
+
+
+
+
+//    return 0;
+//}
+//extern int Add(int, int);
+//int main(void)
+//{
+//    int a = 10;
+//    int b = 20;
+//    int c = Add(a,b);
+//    printf("%d\n",c);
+//
+//    return 0;
+//}
+
+//#define max 100 (定义标识符常量，宏--带参数）
+//#define Max(x,y) (x>y?x:y)
+//int main(void)
+//{
+//    int a = 10;
+//    int b = 20;
+//    int max =Max(a,b);
+//    printf("%d\n",max);
+//    return 0;
+//}
+// int main()
+// {
+//     int a = 10;
+//     int* p = &a;
+//     *p = 20;
+//     printf("a = %d\n",a);
+//     return 0;
+// }
+//int main
+//{
+//    return 0;
+
+//}
+//int main()
+//{
+//    char ch = 'w';
+//    char*pc = &ch;
+//    *pc = 'z';
+//    printf("%c\n",ch);
+//    return 0;
+//}
+//int main(void)
+//{
+//    printf("    **    \n");
+//    printf("    **    \n");
+//    return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 91.c
@@ -0,0 +1,6 @@
+
+//int Add(int x,int y)
+//{
+//    int z = x + y;
+//    return z;
+//}
