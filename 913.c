28b4f6bad5f0ecf3ac16efbbcd56c1de4c9eb85c
w1ouo1w <143856486+w1ouo1w@users.noreply.github.com>
2023-09-13 23:50:07 +0800

    Add files via upload

---
+++ 913.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "add.h"
+
+/*int main(void)
+{
+    int a = 0;
+    int b = 10;
+    int c = Add(a,b);
+    printf("%d",c);
+    return 0;
+}*/
+
+
+//ตÝน้
+
+/*int main(void)
+{
+    printf("hehe");
+    main();
+    return 0;
+}*/
+
---
+++ add.h
@@ -0,0 +1,9 @@
+#ifndef __ADD_H__//如果没有定义过
+#define __ADD_H__//定义
+
+//函数的声明
+int Add(int x,int y);
+
+
+
+#endif // __ADD_H__
